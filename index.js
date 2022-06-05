a983ee1e8efdcf43e9f4238ec8d116e430aceb2d
George Jauga <georgejauga37@gmail.com>
2022-06-05 10:59:09 +0300

    commit tree d5e9085730e1c851bc49a94d2a07ec12abc091cd

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Follow along with the examples here
+// Follow along with the examples h
+function doSomething(thing) {
+    console.log(thing);
+  }
+  function sayHelloTo(firstName) {console.log(`Hello, ${firstName}!`)
+}
+sayHelloTo("Guadalupe")
+function say(greeting,firstName) {console.log(`${greeting}, ${firstName}`)}
+say ("hello", "Jijo")
+function add(x , y){ x + y}
+console.log(add(1 ,2))
+add( 9000 ,80) 
+console.log(add(9000 ,80))
